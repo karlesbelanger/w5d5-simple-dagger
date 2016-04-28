75f40ff08ebf02ac355aa8b2e58666360319a1b8
Evin1- <edwin.hdz.12.0@gmail.com>
2016-04-28 13:47:12 -0400

    Add App and SharedPreferences modules

---
+++ app/src/main/java/com/example/daggersimple/SharedModule.java
@@ -0,0 +1,33 @@
+package com.example.daggersimple;
+
+import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+/**
+ * Created by evin on 4/28/16.
+ */
+@Module
+public class SharedModule {
+
+    String mBaseUrl;
+
+    // Constructor needs one parameter to instantiate.
+    public SharedModule(String baseUrl) {
+        this.mBaseUrl = baseUrl;
+    }
+
+    // Dagger will only look for methods annotated with @Provides
+    @Provides
+    @Singleton
+    // Application reference must come from AppModule.class
+    SharedPreferences providesSharedPreferences(Application application) {
+        return PreferenceManager.getDefaultSharedPreferences(application);
+    }
+
+}
