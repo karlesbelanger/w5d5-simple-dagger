45b6fb4cb185730b405e6664672bd0904a4f4a89
Evin1- <edwin.hdz.12.0@gmail.com>
2016-04-28 13:48:41 -0400

    Optimize imports

--- app/src/main/java/com/example/daggersimple/MainActivity.java
+++ app/src/main/java/com/example/daggersimple/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.daggersimple;
 
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
